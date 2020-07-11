fc120a4b0b0beaf85ffe488d4e1887f4e75d2a6d
Deepanshu <deepanshu111197@gmail.com>
2020-07-11 18:32:22 +0530

    10.1

--- employee management system/ems/settings.py
+++ employee management system/ems/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'ems.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'ems', 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
