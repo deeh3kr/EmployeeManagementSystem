7714d332d26cf8bfdc462378d76fd41475a5c10a
Deepanshu <deepanshu111197@gmail.com>
2020-07-11 18:02:36 +0530

    Video 9

--- employee management system/employee/models.py
+++ employee management system/employee/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
 class Profile(models.Model):
+    #OneToOne mean whenever new user is created, a new profile will be created
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     designation = models.CharField(max_length = 20, null = False, blank = False)
     salary = models.IntegerField(null = True, blank = True)
@@ -13,3 +16,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return "{0} {1}".format(self.user.first_name, self.user.last_name)
+
+
+#if a new user is created, then new profile will automatically be created
+# it is called signal receiver system
+@receiver(post_save, sender = User)
+def user_is_created(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
+    else:
+        instance.profile.save()
